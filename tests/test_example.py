e01961e606243b2860c9df1215bbfbdcb81b1066
alchemmist <anton.ingrish@gmail.com>
2026-02-12 07:08:48 +0300

    fix test example, for correct code coverage

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import pytest
+from src.main import main
+
+@pytest.mark.asyncio
+async def test_example():
+    """
+    This is example just for you know,
+    how to write a tests with async too.
+    You can import from your source code
+    any function or class and test it here.
+
+    Function main imported only for example
+    of importing. When you start to write tests,
+    delete this file totally.
+    """
--- tests/test_main.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.main import main
-
-@pytest.mark.asyncio
-async def test_main_example():
-    result = await main()
-    assert result is None
