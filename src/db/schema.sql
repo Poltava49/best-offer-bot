4619200998c28024b084cfaef689c9a5ff77544c
Poltava49 <arturkorolartur49@gmail.com>
2026-02-21 14:32:05 +0300

    fix imports, exceptions and etc

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,33 +1,25 @@
 -- Расширение для UUID
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-
-
 -- Таблица пользователей Telegram
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT  uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     telegram_id BIGINT UNIQUE NOT NULL,
     username VARCHAR(100),
     is_admin BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
-
-
-
 -- Таблица логирования парсинга
 CREATE TABLE IF NOT EXISTS parsing_logs (
-    id UUID PRIMARY KEY DEFAULT  uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     marketplace_id INTEGER NOT NULL,
     product_id UUID NOT NULL
 );
 
-
-
-
+-- Таблица кэша
 CREATE TABLE IF NOT EXISTS cache (
-    id UUID PRIMARY KEY DEFAULT  uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     request_url TEXT NOT NULL,
     request_body TEXT NULL,
     response_status_code INTEGER NOT NULL,
