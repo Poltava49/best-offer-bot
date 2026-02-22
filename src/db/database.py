aab039231af075bd870fe0c09f87b30fe368fa61
Poltava49 <arturkorolartur49@gmail.com>
2026-02-22 21:01:02 +0300

    fix security vulnerability in db connect

--- src/db/database.py
+++ src/db/database.py
@@ -1,13 +1,16 @@
 import psycopg
 from psycopg import Connection
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def connect_to_db() -> Connection:
     conn = psycopg.connect(
-        host="localhost",
-        port=5430,
-        dbname="parser_bot_db",
-        user="admin",
-        password="qwerty",
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
     )
     return conn
