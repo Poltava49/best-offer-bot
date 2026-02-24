5388594575737873c0f3d1a9c9ee8ee9b0166055
Poltava49 <arturkorolartur49@gmail.com>
2026-02-24 23:03:22 +0300

    fix russian logging

--- tests/test_telegram_bot.py
+++ tests/test_telegram_bot.py
@@ -1,12 +1,12 @@
 import pytest
 from telegram import Update, Message
-from telegram.ext import ContextTypes
-from src.app.telegram_bot import start, stop, parsing, info, handle_text
+from telegram.ext import ContextTypes, Application, CommandHandler, MessageHandler, filters
+from app.telegram_bot import start, stop, parsing, info, handle_text
+from exceptions import MessageHandlerBotError
 
 
 @pytest.fixture
 def mock_update(mocker):
-    # Создаём фиктивные методы
     update = mocker.MagicMock(spec=Update)
     mock_message = mocker.AsyncMock(spec=Message)
     mock_message.reply_text = mocker.AsyncMock()
@@ -17,13 +17,11 @@ def mock_update(mocker):
 
 @pytest.fixture
 def mock_context(mocker):
-    # Создаём фиктивный context
     return mocker.MagicMock(spec=ContextTypes.DEFAULT_TYPE)
 
 
 @pytest.fixture
 def mock_start_markup(mocker):
-    # Создаём фиктивную клавиатуру
     markup = mocker.MagicMock()
     mocker.patch("src.app.telegram_bot.start_markup", markup)
     return markup
@@ -31,7 +29,6 @@ def mock_start_markup(mocker):
 
 @pytest.fixture
 def mock_stop_markup(mocker):
-    # Фикстура для подмены stop_markup
     markup = mocker.MagicMock()
     mocker.patch("src.app.telegram_bot.stop_markup", markup)
     return markup
@@ -50,6 +47,9 @@ async def test_start_bot(mock_update, mock_context, mock_start_markup):
         "и я начну мониторить конкурентов, скидки и динамику. Данные — твоя суперсила!",
         reply_markup=mock_start_markup,
     )
+    with pytest.raises(MessageHandlerBotError):
+        mock_update.message = None
+        await start(mock_update, mock_context)
 
 
 @pytest.mark.asyncio
@@ -61,6 +61,9 @@ async def test_stop(mock_update, mock_context):
     mock_update.message.reply_text.assert_awaited_once()
     args, kwargs = mock_update.message.reply_text.await_args
     assert args[0] == "Пока! Клавиатура удалена."
+    mock_update.message = None
+    with pytest.raises(MessageHandlerBotError):
+        await stop(mock_update, mock_context)
 
 
 @pytest.mark.asyncio
@@ -72,6 +75,9 @@ async def test_parsing(mock_update, mock_context, mock_stop_markup):
     mock_update.message.reply_text.assert_awaited_once_with(
         "Парсинг запущен...", reply_markup=mock_stop_markup
     )
+    mock_update.message = None
+    with pytest.raises(MessageHandlerBotError):
+        await parsing(mock_update, mock_context)
 
 
 @pytest.mark.asyncio
@@ -83,6 +89,9 @@ async def test_info(mock_update, mock_context, mock_stop_markup):
     mock_update.message.reply_text.assert_awaited_once_with(
         "Информация о боте...", reply_markup=mock_stop_markup
     )
+    with pytest.raises(MessageHandlerBotError):
+        mock_update.message = None
+        await info(mock_update, mock_context)
 
 
 @pytest.mark.asyncio
@@ -95,3 +104,6 @@ async def test_handle_text(mock_update, mock_context):
     mock_update.message.reply_text.assert_awaited_once_with(
         f"Вы сказали: {text_message}"
     )
+    with pytest.raises(MessageHandlerBotError):
+        mock_update.message = None
+        await handle_text(mock_update, mock_context)
