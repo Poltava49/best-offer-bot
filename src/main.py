a977ec2f03757b4f2cc2c30d46fbf563af741959
Poltava49 <arturkorolartur49@gmail.com>
2026-02-24 23:03:22 +0300

    fix russian logging

--- src/main.py
+++ src/main.py
@@ -1,34 +1,40 @@
 import os
 import asyncio
 import sys
-from src.db.database import connect_to_db
-from src.app.telegram_bot import run_bot
+from db.database import connect_to_db
+from app.telegram_bot import run_bot
 from typing import cast
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not isinstance(BOT_TOKEN, str):
-    print("BOT_TOKEN не найден в .env файле")
+    logger.info("BOT_TOKEN not found in .env file")
     sys.exit(1)
 
 
 async def main() -> None:
     """Async main func"""
-    print("Запуск бота-парсера маркетплейсов...")
+    logger.info("Launching a marketplace parser bot...")
     try:
         connect_to_db()
-        print("Подключение к PostgreSQL успешно!")
+        logger.info("Connection to PostgreSQL successful!")
 
     except Exception as e:
-        print(f"Ошибка подключения к базе - {e}")
+        logger.info(f"Error connecting to database - {e}")
 
     # Запуск бота
     try:
         await run_bot(cast(str, BOT_TOKEN))
     except KeyboardInterrupt:
-        print("Остановка бота...")
+        logger.info("Stopping the bot...")
     except Exception as e:
-        print(f"Ошибка в боте: {e}")
+        logger.info(f"Error in bot: {e}")
 
 
 if __name__ == "__main__":
